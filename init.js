7babc8ac2f47c9303f8b5335ecfda83165eb9676
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-19 22:20:51 +0700

    Add functional calculator
    plus-minus '+/-' function not working yet.

---
+++ init.js
@@ -0,0 +1,41 @@
+createButtons();
+
+function createButtons() {
+  const buttonsToCreate = [
+    "ce",
+    "c",
+    "back",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "+/-",
+    "0",
+    ".",
+    "=",
+  ];
+  const buttonsContainer = document.querySelector(".buttons-container");
+  let buttonsIndex = 0;
+  for (let i = 0; i < 5; i++) {
+    const hGrid = document.createElement("div");
+    hGrid.classList.add("h-grid");
+    for (let j = 0; j < 4; j++) {
+      const button = document.createElement("button");
+      button.innerHTML = buttonsToCreate[buttonsIndex].toUpperCase();
+      button.dataset.key = buttonsToCreate[buttonsIndex];
+      button.classList.add("button-key");
+      hGrid.appendChild(button);
+      buttonsIndex++;
+    }
+    buttonsContainer.appendChild(hGrid);
+  }
+}
---
+++ script.js
@@ -0,0 +1,110 @@
+const operationDisplay = document.querySelector(".operation");
+const resultDisplay = document.querySelector(".result");
+const operator = ["/", "x", "+", "-"];
+
+getButtonKey();
+
+let toCalculate = ["0", "", "0"];
+let toCalculateIndex = 0;
+let reset = false;
+
+function updateDisplay() {
+  operationDisplay.innerHTML = toCalculate[0] + " " + toCalculate[1];
+
+  resultDisplay.innerHTML =
+    toCalculateIndex === 0 ? toCalculate[0] : toCalculate[2];
+}
+
+function storeVariable(key) {
+  if (toCalculate[0] === Infinity) {
+    toCalculate[0] = "0";
+    toCalculateIndex = 0;
+  }
+  if (reset) {
+    toCalculate = ["0", "", "0"];
+    toCalculateIndex = 0;
+    reset = false;
+  }
+
+  if (!isNaN(key)) {
+    handleNumber(key);
+  } else if (operator.includes(key)) {
+    operate();
+    toCalculateIndex = 2;
+    toCalculate[1] = key;
+  } else {
+    otherOperate(key);
+  }
+}
+
+function handleNumber(key) {
+  toCalculate[toCalculateIndex] === "0"
+    ? (toCalculate[toCalculateIndex] = key)
+    : (toCalculate[toCalculateIndex] += key);
+}
+
+function otherOperate(key) {
+  switch (key) {
+    case "=":
+      operate();
+      toCalculate = ["&nbsp", "", toCalculate[0]];
+      reset = true;
+      break;
+    case "c":
+      toCalculate = ["0", "", "0"];
+      toCalculateIndex = 0;
+      break;
+    case "ce":
+      toCalculate[toCalculateIndex] = "0";
+      break;
+    case "back":
+      toCalculate[toCalculateIndex] = toCalculate[toCalculateIndex].slice(
+        0,
+        -1
+      );
+      toCalculate[toCalculateIndex] =
+        toCalculate[toCalculateIndex] === ""
+          ? "0"
+          : toCalculate[toCalculateIndex];
+      break;
+    case "+/-":
+      break;
+    case ".":
+      toCalculate[toCalculateIndex] += key;
+      break;
+  }
+}
+
+function operate() {
+  var result = 0;
+  switch (toCalculate[1]) {
+    case "+":
+      result = parseFloat(toCalculate[0]) + parseFloat(toCalculate[2]);
+      toCalculate[0] = parseFloat(result);
+      break;
+    case "-":
+      result = parseFloat(toCalculate[0]) - parseFloat(toCalculate[2]);
+      toCalculate[0] = parseFloat(result);
+      break;
+    case "x":
+      result = parseFloat(toCalculate[0]) * parseFloat(toCalculate[2]);
+      toCalculate[0] = parseFloat(result);
+      break;
+    case "/":
+      result = parseFloat(toCalculate[0]) / parseFloat(toCalculate[2]);
+      toCalculate[0] = parseFloat(result);
+      break;
+  }
+  toCalculate[2] = "0";
+}
+
+function getButtonKey() {
+  const buttonsContainer = document.querySelectorAll(".button-key");
+  buttonsContainer.forEach((button) => {
+    button.addEventListener("click", () => {
+      storeVariable(button.dataset.key);
+      updateDisplay();
+      console.log(button.dataset.key, toCalculate, toCalculateIndex);
+    });
+  });
+}
